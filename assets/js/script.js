5be087f5744720d62016956865a5cc8073256e35
Candra Fried <candracodes@gmail.com>
2021-09-02 17:36:21 -0500

    Adding commentary on how to approach setting and getting input value for local storage. Also removed button element from it's parent div to make traversing the DOM a bit easier.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var currentDayEl = $('#currentDay');
 
 // Display Current Day and Time
 function displayCurrentTime() {
-  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  // var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  var rightNow = moment().format('MMM DD, YYYY [at] HH');
   currentDayEl.text(rightNow);
 }
+//TODO: You really just need a variable for HH to do tense comparison
 
 // call displayCurrentTime function to display seconds in real time
 setInterval(displayCurrentTime, 1000);
@@ -20,27 +22,31 @@ setInterval(displayCurrentTime, 1000);
 
 function someFunction(){
 
-// for every ID that has .time-block ... check to see what the number ID, and if that id is > the current time, add a class
-// of past
-
-// if THIS is clicked, then store locally, and setItem locally
 
 }
 
+var $timeKeeper = [];
 
 // Event Listener for Save Button
 $(".saveBtn").on("click", function () {
   // TODO: figure out the syntax to say when "THIS" button is clicked, only grab the value of THIS input field
-  // console.log(this);
-  // Get nearby values of the description in JQuery
 
-  var $text = $(".description").val();
+  // duh Candra, you need an empty array  to collect all this junk, instead of obliterating everything
 
-  console.log($(this).siblings("input").val());
+  // var $text = $(".description").val();
+  var $thisText = $(this).siblings("input").val();
+
+  $timeKeeper.push($thisText)
+  console.log($timeKeeper);
 
   // Save text in local storage
-  localStorage.setItem("text", $text);
+  localStorage.setItem("text", $thisText);
   // console.log("There's got to be an easier way to save every single description like: " + $text);
+
+  //TODO LIST:
+  // add a mechanism to find the ID relative to the button click
+  // store in an object so you can pair where it came from AS WELL as the value of the content
+  // assign a class based on the value being greater or less than the current time
 });
 
 // TODO: my plan is to store every entered value so that it stays on the screen, even after refresh. Possible getItem and then setItem? Explore more...
