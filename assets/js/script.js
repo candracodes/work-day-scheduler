d21ff141e73c0f436fe5ba06b8f6fa237e3c1d6e
Candra Fried <candracodes@gmail.com>
2021-09-03 16:40:08 -0500

    Successfully assigned color coding based on past, present and future tense. Still need to write code that both gets AND sets local storage entered values so that they remain on the page. even after refresh.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,74 @@
-// DEFINE HTML IDS AS VARIABLES
+// DEFINE HTML DATE DIV ID AS A VARIABLE
 var currentDayEl = $('#currentDay');
 
-// Display Current Day and Time
+// Create a function that handles the displaying of present day + time
 function displayCurrentTime() {
-  // var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-  var rightNow = moment().format('MMM DD, YYYY [at] HH');
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   currentDayEl.text(rightNow);
 }
-//TODO: You really just need a variable for HH to do tense comparison
 
-// call displayCurrentTime function to display seconds in real time
+// Call displayCurrentTime function to display seconds in real time
 setInterval(displayCurrentTime, 1000);
 
-// TODO: Write a function that 1) does a loop to go through every time-slot div and figures out if the time of that div is greater than the current time, and if so, add attr of past/present/future/etc
+// Defining a variable for future use for time comparison
+var currentHourEl = moment().hour();
+console.log("The hour is currently: " + currentHourEl + " in military time.");
 
-// Function should have maybe some sort of loop
-// for (var i = 0; i < someParameter.length; i++) {
-//   // Do something
-  
-// }
+// Define a function that handles the color association for past, present and future tense
+function timeTracker() {
 
-function someFunction(){
+  // Run a loop that examines present time, and assigns a color
+  $(".description").each(function () {
 
+    // remove the "hour" from the string, and leave the associated number
+    var $timeBlockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // if time is in the past
+    if ($timeBlockTime < currentHourEl) {
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+    }
+    // if time is in the present
+    else if ($timeBlockTime === currentHourEl) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    }
+    // if time is in the future
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+
+  })
 
 }
 
+// Call timeTraacker function to display tense color attributes
+timeTracker();
+
+// Creating a global, empty array as a means of storing the entered values from the saveBtn even listener
 var $timeKeeper = [];
 
 // Event Listener for Save Button
 $(".saveBtn").on("click", function () {
-  // TODO: figure out the syntax to say when "THIS" button is clicked, only grab the value of THIS input field
 
-  // duh Candra, you need an empty array  to collect all this junk, instead of obliterating everything
-
-  // var $text = $(".description").val();
+  // Capture THIS value entered in this specific input field by traversing the DOM
   var $thisText = $(this).siblings("input").val();
 
+  // Capture the ID of THIS particular saveBtn
+  var $thisButtonTime = $(this).attr("id");
+
+  // Add the previously entered value to the end of the timeKeeper array
   $timeKeeper.push($thisText)
   console.log($timeKeeper);
-
-  // Save text in local storage
-  localStorage.setItem("text", $thisText);
-  // console.log("There's got to be an easier way to save every single description like: " + $text);
+  console.log("You've just clicked on the " + $thisButtonTime + " o'clock button");
 
   //TODO LIST:
   // add a mechanism to find the ID relative to the button click
   // store in an object so you can pair where it came from AS WELL as the value of the content
   // assign a class based on the value being greater or less than the current time
+
 });
-
-// TODO: my plan is to store every entered value so that it stays on the screen, even after refresh. Possible getItem and then setItem? Explore more...
-// Get values from each time slot
-// var now = new Date().getHours();
-// if (now > 9) {
-//   $("#comment9am").addClass("past");
-// }
-// $("#hour5 .description").val(localStorage.getItem("hour5"));
-
-// TODO: Come up with a better idea other than giving EACH save button it's own id to parse/get/set/store the accompanying value; there's got to be an easy, shorter code
